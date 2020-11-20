3b915988e2916cff14fc1988721ee5b05369dd2f
Ivan Borba <mrborbaivan@gmail.com>
2020-11-20 09:08:19 -0300

    added the authenticated page

---
+++ src/data/AuthenticatedData.js
@@ -0,0 +1,22 @@
+export const AuthenticatedData = [
+  {
+    label: "Nunca",
+    url: "authenticated/users",
+  },
+  {
+    label: "Mais",
+    url: "authenticated/feedbacks",
+  },
+  {
+    label: "Comer",
+    url: "authenticated/feedback-form",
+  },
+  {
+    label: "Torresmo",
+    url: "authenticated/contact",
+  },
+  {
+    label: "Log out",
+    url: "/",
+  },
+];
